dcb1f45d221c369617c21828800ddb98f9f34301
nthskyradiated <andy.pandaan@outlook.com>
2023-10-09 22:17:07 +0400

    Finished Crud for Client side UI

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('colors')
 require ('dotenv').config()
 const { graphqlHTTP } = require('express-graphql')
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000
 const app = express();
 
 connectDB();
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
